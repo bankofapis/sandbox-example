90bb3506872959ba723c8ed3aba5c1eead00e45f
Olly Hayes <olly.hayes@gmail.com>
2019-03-25 14:04:02 +0000

    Move api methods to a separate file

---
+++ api.js
@@ -0,0 +1,101 @@
+const config = require('./config.json');
+const request = require('request-promise-native').defaults({
+	strictSSL: false,
+	json: true,
+	proxy: config.proxy
+});
+
+async function retrieveAccessToken(authorisationCode = null) {
+	const response = await request({
+		uri: 'https://ob.rbs.useinfinite.io/token',
+		method: 'POST',
+		qs: {
+			grant_type: authorisationCode
+				? 'authorization_code'
+				: 'client_credentials',
+			client_id: config.clientId,
+			client_secret: config.clientSecret,
+			code: authorisationCode,
+			scope: 'accounts'
+		},
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+	});
+
+	return response.access_token;
+}
+
+async function createAccountAccessConsent(accessToken) {
+	const response = await request({
+		uri: 'https://ob.rbs.useinfinite.io/open-banking/v3.1/aisp/account-access-consents',
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${accessToken}`,
+			'x-fapi-financial-id': '0015800000jfwB4AAI'
+		},
+		body: {
+			Data: {
+				Permissions: [
+					'ReadAccountsDetail',
+					'ReadBalances',
+					'ReadTransactionsCredits',
+					'ReadTransactionsDebits',
+					'ReadTransactionsDetail'
+				]
+			},
+			Risk: {}
+		}
+	});
+
+	return response.Data.ConsentId;
+}
+
+async function authoriseProgramatically(consentId) {
+	const response = await request({
+		uri: 'https://api.rbs.useinfinite.io/authorize',
+		method: 'GET',
+		qs: {
+			client_id: config.clientId,
+			response_type: 'code id_token',
+			scope: 'openid accounts',
+			redirect_uri: 'https://9051fb9d-1c3c-4c75-b036-dcbb662e3e7f.example.org/redirect',
+			state: 'ABC',
+			request: consentId,
+			authorization_mode: 'AUTO_POSTMAN',
+			authorization_result: 'APPROVED',
+			authorization_username: '123456789012@9051fb9d-1c3c-4c75-b036-dcbb662e3e7f.example.org',
+		}
+	});
+
+	const { redirectUri } = response;
+
+	// get AUTH_CODE from: https://domain/path#p1=v1&code=AUTH_CODE&p3=v3
+	const [ , fragmentIdentifier ] = redirectUri.split('#');
+	const [ , authorizationCode ] = fragmentIdentifier
+		.split('&')
+		.map(parameter => parameter.split('='))
+		.find(([key]) => key === 'code');
+
+	return authorizationCode;
+}
+
+async function getAccounts(accessToken) {
+	const response = await request({
+		uri: 'https://ob.rbs.useinfinite.io/open-banking/v3.1/aisp/accounts',
+		method: 'GET',
+		headers: {
+			Authorization: `Bearer ${accessToken}`,
+			'x-fapi-financial-id': '0015800000jfwB4AAI'
+		}
+	});
+
+	return response.Data.Account;
+}
+
+module.exports = {
+	retrieveAccessToken,
+	createAccountAccessConsent,
+	authoriseProgramatically,
+	getAccounts
+};
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const config = require('./config.json');
-const requestBase = require('request-promise-native');
+const {
+	retrieveAccessToken,
+	createAccountAccessConsent,
+	authoriseProgramatically,
+	getAccounts
+} = require('./api');
 
-const request = requestBase.defaults({
-	strictSSL: false,
-	json: true,
-	proxy: config.proxy
-});
+automaticAuthorisation();
 
-(async function() {
+async function automaticAuthorisation() {
 	console.log('Getting initial access token...');
 	const accessToken = await retrieveAccessToken();
 
@@ -25,92 +25,24 @@ const request = requestBase.defaults({
 
 	console.log('Accounts:');
 	console.log(JSON.stringify(accounts, null, 4));
-})();
-
-async function retrieveAccessToken(authorisationCode = null) {
-	const response = await request({
-		uri: 'https://ob.rbs.useinfinite.io/token',
-		method: 'POST',
-		qs: {
-			grant_type: authorisationCode
-				? 'authorization_code'
-				: 'client_credentials',
-			client_id: config.clientId,
-			client_secret: config.clientSecret,
-			code: authorisationCode,
-			scope: 'accounts'
-		},
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded'
-		},
-	});
-
-	return response.access_token;
 }
 
-async function createAccountAccessConsent(accessToken) {
-	const response = await request({
-		uri: 'https://ob.rbs.useinfinite.io/open-banking/v3.1/aisp/account-access-consents',
-		method: 'POST',
-		headers: {
-			Authorization: `Bearer ${accessToken}`,
-			'x-fapi-financial-id': '0015800000jfwB4AAI'
-		},
-		body: {
-			Data: {
-				Permissions: [
-					'ReadAccountsDetail',
-					'ReadBalances',
-					'ReadTransactionsCredits',
-					'ReadTransactionsDebits',
-					'ReadTransactionsDetail'
-				]
-			},
-			Risk: {}
-		}
-	});
+// async function manualAuthorisation() {
+// 	console.log('Getting initial access token...');
+// 	const accessToken = await retrieveAccessToken();
 
-	return response.Data.ConsentId;
-}
+// 	console.log('Creating consent...');
+// 	const consentId = await createAccountAccessConsent(accessToken);
 
-async function authoriseProgramatically(consentId) {
-	const response = await request({
-		uri: 'https://api.rbs.useinfinite.io/authorize',
-		method: 'GET',
-		qs: {
-			client_id: config.clientId,
-			response_type: 'code id_token',
-			scope: 'openid accounts',
-			redirect_uri: 'https://9051fb9d-1c3c-4c75-b036-dcbb662e3e7f.example.org/redirect',
-			state: 'ABC',
-			request: consentId,
-			authorization_mode: 'AUTO_POSTMAN',
-			authorization_result: 'APPROVED',
-			authorization_username: '123456789012@9051fb9d-1c3c-4c75-b036-dcbb662e3e7f.example.org',
-		}
-	});
+// 	console.log(`Consent ID: ${consentId}. Authorising automatically...`);
+// 	const authorisationCode = await authoriseProgramatically(consentId, accessToken);
 
-	const { redirectUri } = response;
+// 	console.log('Retreiving authorised access token...');
+// 	const authorisedAccesToken = await retrieveAccessToken(authorisationCode);
 
-	// get AUTH_CODE from: https://domain/path#p1=v1&code=AUTH_CODE&p3=v3
-	const [ , fragmentIdentifier ] = redirectUri.split('#');
-	const [ , authorizationCode ] = fragmentIdentifier
-		.split('&')
-		.map(parameter => parameter.split('='))
-		.find(([key]) => key === 'code');
+// 	console.log('Retrieving users accounts...');
+// 	const accounts = await getAccounts(authorisedAccesToken);
 
-	return authorizationCode;
-}
-
-async function getAccounts(accessToken) {
-	const response = await request({
-		uri: 'https://ob.rbs.useinfinite.io/open-banking/v3.1/aisp/accounts',
-		method: 'GET',
-		headers: {
-			Authorization: `Bearer ${accessToken}`,
-			'x-fapi-financial-id': '0015800000jfwB4AAI'
-		}
-	});
-
-	return response.Data.Account;
-}
+// 	console.log('Accounts:');
+// 	console.log(JSON.stringify(accounts, null, 4));
+// }
